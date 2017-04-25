21df6146bc9a3757dc39cec2e55017d311642e26
Alexpham688 <Alexpham688@gmail.com>
2017-04-25 18:41:15 -0400

    GITHUB made me commit

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
   var form = $('form');
       form.hide();
-      form.slideDown(8000);
-
+      form.slideDown(8000).fadeOut(8000);
+     ///// alex added slideUp
 
 
 });
