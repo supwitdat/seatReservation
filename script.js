cb6b30f2cd527d60cd328c7b7b13160bcadd0dd0
Brian Barnes <barnesb7@gmail.com>
2017-04-25 17:47:26 -0400

    first form animation

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ $(document).ready(function(){
 
   var form = $('form');
       form.hide();
-      form.slideDown(8000);
+      form.slideDown(3000);
+      form.fadeTo(3000, 0.0);
 
+    var seat = $('.seat');
+    seat.on('click',function(){
+        form.fadeTo(2000, 1.0);
+    });
 
+    // seat.mouseleave(function(){
+    //   form.fadeTo(1000, 0.0);
+    //
+    // });
 
 });
